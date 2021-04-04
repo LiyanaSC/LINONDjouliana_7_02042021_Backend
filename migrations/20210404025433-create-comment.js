3b5215c94721a3002455b5c9bb83dfc231533664
OPBS Liyana <liyana.perso@gmail.com>
2021-04-03 23:07:57 -0400

    mise en place modèle

---
+++ migrations/20210404025433-create-comment.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+    up: async(queryInterface, Sequelize) => {
+        await queryInterface.createTable('Comments', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            userId: {
+                allowNull: false,
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'Users',
+                    key: 'id'
+                }
+            },
+            articleId: {
+                allowNull: false,
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'Articles',
+                    key: 'id'
+                }
+            },
+            content: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            publicationDate: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: async(queryInterface, Sequelize) => {
+        await queryInterface.dropTable('Comments');
+    }
+};
